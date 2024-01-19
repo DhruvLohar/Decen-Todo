c016d8eb3031c021608d81a21b311078bafd0098
DhruvLohar <dhruvlohar09@gmail.com>
2024-01-19 21:25:18 +0530

    Added UI components

---
+++ components/CreateTodo.jsx
@@ -0,0 +1,13 @@
+export default ({ isOpen, setOpen }) => {
+    return (
+        <div className="
+            flex items-center justify-center
+            absolute left-0
+            w-[32vw] h-[90vh] 
+            rounded-r-[3rem]
+            bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10
+        ">
+            Create kar bhai
+        </div>
+    )
+}
--- components/TodoCard.jsx
+++ components/TodoCard.jsx
@@ -1,5 +1,38 @@
-export default () => {
+import { TickCircle } from "iconsax-react"
+
+export default ({ todo }) => {
+
+    const editTodo = () => {}
+
+    const deleteTodo = () => {}
+
+    const markAsComplete = () => {}
+
     return (
-        <h1 className="text-lg">card hai</h1>
+        <div 
+            style={{boxShadow: "5px 5px 0px white"}}
+            className="
+                w-full
+                flex items-center justify-start
+                card border border-2 py-4 px-6 rounded-3xl
+            "
+        >
+            <h1 className="card-title mr-2">Fix Pitstop Team Registration</h1>
+            <div className="flex items-center ml-auto todo-actions space-x-4 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={32} height={32}>
+                    <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
+                </svg>
+                <hr className="rotate-90 bg-white w-[1.8rem] opacity-30" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={28} height={28}>
+                    <path d="M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"></path>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={28} height={28}>
+                    <path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={24} height={24}>
+                    <path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path>
+                </svg>
+            </div>
+        </div>
     )
 }
